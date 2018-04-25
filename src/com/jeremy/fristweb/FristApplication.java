88051e2a363c129cf333b05a0e9318e91526bb89
LD-201712211137\Administrator <296953200@qq.com>
2018-04-25 17:43:57 +0800

    第一次提交

---
+++ src/com/jeremy/fristweb/FristApplication.java
@@ -0,0 +1,13 @@
+package com.jeremy.fristweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FristApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FristApplication.class, args);
+    }
+
+}
---
+++ src/com/jeremy/fristweb/HelloController.java
@@ -0,0 +1,17 @@
+package com.jeremy.fristweb;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/abc", method = RequestMethod.GET)
+    @ResponseBody
+    public String hellobiubiu() {
+        return "kaokaokaokao";
+    }
+
+}
